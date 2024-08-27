eebe906c91bb1930b990fb46df026f0b3bc01d67
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 12:09:13 +0700

    blok program

---
+++ blok program .php
@@ -0,0 +1,9 @@
+<?php
+$umur=18;{
+    echo "<p>Kamu sudah dewasa";
+    echo "<p>Selamat datang";
+    echo "<p>Kamu boleh minum kopi";
+    echo "<br><br><br><strong>By meylisa eka putry</strong>";
+
+} 
+   
