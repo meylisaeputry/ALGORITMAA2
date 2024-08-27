515fb08189e825647b7e96a76b11e52bc1194492
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 11:05:12 +0700

    penulisan case sensitive

---
+++ 5 penulisan case PHP sensitive .php
@@ -0,0 +1,25 @@
+<?php
+
+$nama = "Meylisa eka putry";
+$NAMA = "meylisa";
+$umur = 16;
+const INI_KONSTANTA = 123;
+$ini_variabel = 23;
+$iniJugaVariabel = 49;
+$iniJugaVariabel;
+
+echo " ======= mencetak variabel ======= ";
+echo "<p>Nama saya adalah $nama";
+echo "<p>Nama panggilan saya adalah $NAMA";
+echo "<p>Umur saya adalah $umur";
+
+echo 
+
+    "<p>Coba pakai blok program",
+    "<p>Nama saya adalah $nama",
+    "<p>Nama panggilan saya  adalah $NAMA",
+    "<p>Umur saya adalah $umur";
+
+    echo "<br><br><br><strong>By meylisa eka putry</strong>";
+
+?>
