85c75255edaa838bc31efd83f46c67835c8572ae
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 16:09:28 +0700

    tipe data

---
+++ tipe data .php
@@ -0,0 +1,28 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+// tipe data string (teks)
+$nama_lengkap = "meylisa eka putry";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 53.3;
+
+// tipe data float
+$tinggi = 160.2;
+
+// tipe data boolean
+$gadis = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "gadis: $gadis";
+
+echo "<br><br><strong>By meylisa eka putry</strong=";
