57f0c3522ba5cf8726bdb625462d6b34ff364017
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 16:30:46 +0700

    tipe data intrger

---
+++ tipe data integer .php
@@ -0,0 +1,8 @@
+<?php
+$nilai = 80; // angka positif
+$poin = -29; // angka negatif
+
+echo "angka positif: $nilai";
+echo "<p>angka negatif: $poin";
+
+echo "<p><strong>By meylisa eka putry</strong=";
