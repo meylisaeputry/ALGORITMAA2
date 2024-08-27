549fa22ad56085808c32af35cca61841b9957f39
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 15:57:27 +0700

    fungsi printf .php

---
+++ fungsi printf .php
@@ -0,0 +1,11 @@
+<?php
+// misalkan kita punya bilangan dengan notasi E seperti ini
+$harga = 500000;
+
+// jika kita cetak dengan echo:
+echo "Harganya adalah Rp $harga";
+
+// jika kita cetak dengan printf
+printf("<p>Harganya adalah Rp %.2f", $harga);
+
+echo "<br><br><br><strong>By meylisa eka putry</strong>";
