0e6b7e32f97fac9813a430463eff4d9dc7ea4e92
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 10:12:27 +0700

    menulis php dalam html

---
+++ 3 menulis php dalam html .php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<html>";
+echo "<head>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Selamat datang</h1>";
+echo "di Smkn 1 karang baru";
+echo "</body>";
+echo "</html>";
+echo "<br><br><strong>By meylisa eka putry</strong=";
