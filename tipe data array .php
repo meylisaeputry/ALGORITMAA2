49381efb5871a189c36c513d65fa8add3c55b395
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 19:01:13 +0700

    tipe data boolean

---
+++ tipe data array .php
@@ -0,0 +1,4 @@
+<?php
+$minuman = array("Kopi", "Teh", "Jus Jeruk");//minuman
+$makanan = ["Nasi Goreng", "Soto", "Bubur"];//makanan
+echo "minuman: $minuman";
---
+++ tipe data bolean .php
@@ -0,0 +1,18 @@
+<?php
+//mendeklarasikan variable boolean 
+$isActive = false;
+$menikah = true;
+//menggunakan boolean dalam peryataan if
+if ($isActive){
+    echo "Nilai ini benar.";
+} else{
+    echo "Nilai ini salah.";
+}
+
+//contoh lain mnggunakan boolean 
+$condition = (5 > 2);
+if ($condition) {
+    echo "<p>5 lebih besar dari 2.";
+} else {
+    echo "5 tidak lebih besar dari 2.";
+}
