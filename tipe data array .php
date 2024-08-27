adbb676795500777c809a28661200ddeb2c46bf7
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 20:11:22 +0700

    tipe data array

---
+++ tipe data array .php
@@ -0,0 +1,30 @@
+<?php
+//cara membuat indexed array
+$minuman = array("<p>Kopi", "<p>Teh", "<p>Jus Jeruk");
+array("kopi");
+array("Teh");
+array("Jus jeruk");
+
+//mengakses elemen array
+echo $minuman  = "<p>kopi"; //output:kopi
+echo $minuman = "<p>Teh"; //output:Teh
+echo $minuman  = "<p>Jus jeruk"; //output:jus jeruk
+//menampilkan seluruh isi array
+print_r($minuman);
+
+$makanan = ["Nasi Goreng", "Soto", "Bubur"];
+array("nasi goreng");
+array("soto");
+array("bubur");
+
+echo $makanan  = "<p>Nasi goreng"; //output:Nasi goreng
+echo $makanan = "<p>soto"; //output:soto
+echo $makanan  = "<p>bubur"; //output:bubur
+
+print_r($makanan);
+
+echo "<p><br><strong>By meylisa eka putry</strong>";
+?>
+
+
+
