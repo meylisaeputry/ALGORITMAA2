e7eb69843dec1e47870715ec1e72b0a203e70950
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 10:37:30 +0700

    penulisan statement dan ekspresi

---
+++ 4 Penulisan Statement dan Ekspresi .php
@@ -0,0 +1,23 @@
+<?php
+echo "ini statement 1";
+echo "ini statement 2";
+//deklarasi sebuah variabel
+$b=10;
+$c=5;
+//ekspresi
+$a1 = $b * $c;
+$a2 = $b + $c;
+//cetak ekspresi
+echo "<p> ini ekspresi -> $a1 = $b * $c";
+echo "<p> ini ekspresi -> $a2 = $b + $c";
+echo "<p>yaitu : .$a1";
+echo "<p>yaitu : .$a2";
+echo "<p><p>";
+
+
+echo "ini statement 1";
+echo "<p>ini statement 2";
+$a = $b + $c;
+
+echo "<br><br><br><strong>By meylisa eka putry</strong>";
+
