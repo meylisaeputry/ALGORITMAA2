3d8945bca2ed778a7315adaf3a4d2257a63683ce
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-27 22:35:53 +0700

    konversi tipe data

--- konversi tipe data di php .php
+++ konversi tipe data di php .php
@@ -1,9 +1,8 @@
 <?php
-$foo = "1";  // mula-mual $foo dalam bentuk string (ASCII 49)
-$foo *= 2;   // $foo sekarang adalah integer (2)
-$foo = $foo * 1.3;  // $foo sekarang adalah float (2.6)
-$foo = 5 * "10 Little birds"; // $foo sekarang adalah integer (50)
-$foo = 5 * "10 Small birds"; // $foo sekarang adalah integer (50)
+$var = "123.45";
+$intvar = "123";
+$intvar = intval($var);//mengubah menjadi integer
+$floatvar = floatval($var);// mengubah menjadi float
+echo $intvar;//output:123
+echo "<p>$floatvar";//output:123.45
 
-
-?>
